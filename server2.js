1eeed9fc88f62bede060b6ca0350e93209580cf7
houssam chakir <thisischchannel@gmail.com>
2024-08-21 12:15:33 +0100

    middlewear, handlers and POST request

--- server2.js
+++ server2.js
@@ -10,18 +10,53 @@ const users = [
   { id: 3, name: "Jimn Doe" },
 ];
 
-const server = createServer((req, res) => {
+//f/ Middlewear
+
+const logger = (req, res, next) => {
+  console.log(req.method, req.url);
+  next(req, res);
+};
+
+const createUserHandler = (req, res) => {
+  let body = '';
+  req.on("data", (chunk) => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    try {
+      const newUser = JSON.parse(body);
+      users.push(newUser);
+      res.statusCode = 201;
+      res.write(JSON.stringify(newUser));
+      res.end();
+
+    } catch (error) {
+      console.log('error handling post request for new user');
+      res.statusCode = 400
+      res.write(JSON.stringify({'error': 'post new user unsuccessful'}))
+    }
+  });
+};
+
+const requestListener = function (req, res) {
   const url = req.url;
   const method = req.method;
 
   res.setHeader("Content-type", "application/json");
   if (url === "/api/users" && method === "GET") {
     res.write(JSON.stringify(users));
+  } else if (url === "/api/users" && method === "POST") {
+    createUserHandler(req, res);
   } else {
     res.statusCode = 404;
     res.write(JSON.stringify({ error: "Page not found" }));
   }
   res.end();
+};
+
+//* SERVER /////////////////////////////////
+const server = createServer((req, res) => {
+  logger(req, res, requestListener);
 });
 
 server.listen(PORT, () => console.log("server up and running"));
